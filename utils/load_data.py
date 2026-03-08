3b33ab526a6bc09d1998ed60d0ea68cf0f31c018
windy.tsoi <polar.windy@gmail.com>
2026-03-08 11:47:18 +0700

    Add data utils

---
+++ utils/load_data.py
@@ -0,0 +1,32 @@
+import os
+import numpy as np
+import pandas as pd
+
+def load_events():
+    events = pd.read_csv("data/events.csv")
+    gp_list = events["Location"].tolist()
+    return events, gp_list
+
+def load_drivers():
+    drivers = pd.read_csv("data/drivers.csv")
+    driver_list = drivers["Abbreviation"].tolist()
+    return drivers, driver_list
+
+def load_results():
+    results = pd.read_csv("data/results.csv")
+    top_drivers = results[results["Position"] <= 3]
+    return results, top_drivers
+
+def load_laps(grand_prix: str, selected_drivers: list):
+    laps = pd.read_csv("data/laps.csv")
+    
+    is_selected_gp = laps[laps["Location"] == grand_prix]
+    is_selected_drivers = laps[laps["Abbreviation"].isin(selected_drivers)]
+
+    laps = laps[is_selected_gp & is_selected_drivers]
+
+    return laps
+
+
+def load_speed_stat():
+    pass
